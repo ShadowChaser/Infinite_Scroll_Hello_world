a089ccdc4fa52266691645273831e4fd27842bb9
Abhishek <abhishekkundu86@gmail.com>
2022-06-30 23:38:20 +0530

    Completed the infinite scroll

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import FetchImages from './Component/FetchImages';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <FetchImages/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/FetchImages.js
@@ -0,0 +1,83 @@
+import axios from 'axios'
+import React,{useState,useEffect, Fragment} from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
+import 'bootstrap/dist/css/bootstrap.css';  
+
+
+const FetchImages=()=> {
+    const totalCount=30;
+    const [product,setProducts]=useState([]);
+    
+    const imageLimit=5;
+
+    const API_URL="https://dummyjson.com/products"
+    
+    const getProductList=()=>{
+        let skip=Math.ceil(product.length/imageLimit)+5;
+        const queryParam="?skip="+ skip+"&limit="+imageLimit;
+        axios.get(API_URL+queryParam)
+        .then((res)=>{
+            const apiResponse=res.data.products;
+            const mergeData=[...product,...apiResponse]
+            setProducts(mergeData)
+
+        })
+        .catch((err)=>{
+            console.error("Error while loading product")
+        })
+
+       
+    }
+    useEffect(() => {
+        getProductList()
+    }, [])
+
+    const fetchMoreData=()=>{
+        if(product.length<totalCount){
+        getProductList()
+        }
+        
+    }
+
+  return (
+    <Fragment>
+        
+        <div className="container" >
+            <div className="row" >
+
+            <InfiniteScroll
+                    dataLength={product.length}
+                    next={fetchMoreData}
+                    hasMore={product.length<totalCount}
+                    loader={<h4>Loading...</h4>}
+                    
+                >
+            {product.length>0 && product.map((key)=>{
+                
+                return(
+                    <Fragment>
+                        <div className='column'>
+                         <div className='card mt-4 mb-3 pl-3' >
+                            <div className='image-block'>
+                                <img src={key.thumbnail} alt="test" style={{height:"6vw",width:"25vh"}}/>
+                            </div>
+                            <div className='content-block'>
+                                <h5>{key.title}</h5>
+                            </div>
+                        </div>
+                        </div>
+                    </Fragment>
+                )
+            })}
+        </InfiniteScroll>
+                        
+        </div>
+        </div>
+    </Fragment>
+  )
+}
+
+
+export default FetchImages;
+
+ 
